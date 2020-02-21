69bb2336ada9e504d7fa04be414eaad7bca61c86
igorvargass <igorvargass@hotmail.com>
2020-02-20 22:58:13 -0300

    formatação de texto

---
+++ src/main.js
@@ -0,0 +1,14 @@
+function calculate(){
+    var vprojeto = document.getElementById("vprojeto").value
+    var hrtrab = document.getElementById("hrtrab").value
+    var diastrab = document.getElementById("diastrab").value
+    var diasferias = document.getElementById("diasferias").value
+    var x = document.getElementById("resultado");
+
+    var valorHora = (vprojeto / (diastrab * 4 * hrtrab) ) + ( ( diasferias * diastrab * hrtrab ) )
+   
+    
+    console.log(valorHora)
+    document.getElementById("result").innerHTML =+valorHora;
+    
+}
